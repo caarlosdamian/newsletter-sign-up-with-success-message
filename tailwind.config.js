0973ed4c0fa4ffe1cd4f8cdcd161e8d7fde01e21
caarlosdamian <dapecarlos@gmail.com>
2023-11-29 19:52:09 -0600

    init app

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        headerMobile: "url('')",
+        headerDesktop: "url('')",
+        headerTablet: "url('')",
+      },
+      colors: {
+        primary: {
+
+        },
+        secondary: {
+ 
+        },
+      },
+      fontFamily: {
+      
+      },
+    },
+  },
+  plugins: [],
+};
