a90389eb6a28be943859f1a213318dc88f8b197c
caarlosdamian <dapecarlos@gmail.com>
2023-11-30 22:51:30 -0600

    feat(app): added semantic tag

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export const Root = () => {
   return (
-    <div className="bg-primary-paleNavy w-screen h-screen flex items-center justify-center">
+    <main className="bg-primary-paleNavy w-screen h-screen flex items-center justify-center">
       <Outlet />
-    </div>
+    </main>
   );
 };
